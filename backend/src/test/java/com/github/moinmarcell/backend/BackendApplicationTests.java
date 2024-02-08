e5a1cc4724658549d7799927aed40fe0d245208c
Marcell Dechant <marcell.dechant@neuefische.de>
2024-02-08 21:08:29 +0100

    test: Add assertion for contextLoads Test

--- backend/src/test/java/com/github/moinmarcell/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/github/moinmarcell/backend/BackendApplicationTests.java
@@ -3,11 +3,16 @@ package com.github.moinmarcell.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class BackendApplicationTests {
 
+    BackendApplication backendApplication = new BackendApplication();
+
     @Test
     void contextLoads() {
+        assertNotNull(backendApplication);
     }
 
 }
