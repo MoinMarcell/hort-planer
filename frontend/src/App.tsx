3aa572283ce0d57dea27458863ff447977c49c8a
Marcell Dechant <marcell.dechant@neuefische.de>
2024-02-10 13:09:16 +0100

    feat: Add Basic Layout with CustomDialog & Card example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,57 @@
+import Layout from "./components/Layout.tsx";
+import {useState} from "react";
+import CustomDialog from "./components/CustomDialog.tsx";
+
 export default function App() {
+    const [openDialog, setOpenDialog] = useState(false);
     return (
-        <h1>Hort Planer</h1>
+        <Layout>
+            <button id="button-add-event">Ein neues Event erstellen</button>
+            <article className="custom-card-article">
+                <header>
+                    <nav>
+                        <ul>
+                            <li>Montag</li>
+                            <li>10.01.2022</li>
+                            <li>10:00 - 12:00</li>
+                        </ul>
+                    </nav>
+                    <h2 className="custom-card-title">Test Event</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                    convallis libero in dui sollicitudin, id dictum nunc fermentum.
+                    Nulla facilisi. Nullam nec nunc sit amet nunc tincidunt
+                    fermentum. Nulla facilisi. Nullam nec nunc sit amet nunc
+                    tincidunt fermentum.
+                </p>
+                <footer>
+                    <nav>
+                        <ul>
+                            <li>
+                                <button>Details</button>
+                            </li>
+                            <li>
+                                <button className="button-yellow">Bearbeiten</button>
+                            </li>
+                            <li>
+                                <button
+                                    className="button-red"
+                                    onClick={() => setOpenDialog(true)}
+                                >
+                                    Löschen
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
+                </footer>
+            </article>
+            <CustomDialog
+                open={openDialog}
+                close={() => setOpenDialog(false)}
+                title={"Bist du dir sicher?"}
+                description={"Möchtest du das Event wirklich löschen?"}
+            />
+        </Layout>
     )
 }
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header className="container">
+            <nav>
+                <ul>
+                    <li><strong>Hort Planer</strong></li>
+                    <li><a href={"/"}>Startseite</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
