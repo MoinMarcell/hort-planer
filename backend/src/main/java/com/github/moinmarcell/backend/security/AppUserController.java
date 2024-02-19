3963f7ed1f168887030e006f59cca3b6b642f8a7
Marcell Dechant <marcell.dechant@neuefische.de>
2024-02-19 13:39:05 +0100

    chore: Add login endpoint & testing

--- backend/src/main/java/com/github/moinmarcell/backend/security/AppUserController.java
+++ backend/src/main/java/com/github/moinmarcell/backend/security/AppUserController.java
@@ -1,9 +1,14 @@
 package com.github.moinmarcell.backend.security;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -11,6 +16,14 @@ public class AppUserController {
     @GetMapping("/me")
     public String getMe() {
         var securityContext = SecurityContextHolder.getContext();
-        return securityContext.getAuthentication().getName();
+        if (!Objects.equals(securityContext.getAuthentication().getName(), "anonymousUser")) {
+            return securityContext.getAuthentication().getName();
+        }
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No User Logged In");
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return getMe();
     }
 }
--- backend/src/main/java/com/github/moinmarcell/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/github/moinmarcell/backend/security/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.github.moinmarcell.backend.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +21,9 @@ public class SecurityConfig {
     private static final String EVENTS_ENDPOINT = "/api/events";
     private static final String AUTH_ENDPOINT = "/api/auth";
 
+    @Value("${app.url}")
+    private String appUrl;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
@@ -32,7 +35,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .exceptionHandling(c -> c.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(c -> c.authenticationEntryPoint((request, response, authException) -> response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase())))
+                .logout(c -> c.logoutSuccessUrl(appUrl))
                 .authorizeHttpRequests(c -> c
                         .requestMatchers(HttpMethod.POST, EVENTS_ENDPOINT).authenticated()
                         .requestMatchers(HttpMethod.PUT, EVENTS_ENDPOINT + "/*").authenticated()
