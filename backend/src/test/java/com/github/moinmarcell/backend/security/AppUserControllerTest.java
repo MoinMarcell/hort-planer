41bf9e59f408225142b73515bdc362663748b330
Marcell Dechant <marcell.dechant@neuefische.de>
2024-02-19 12:05:41 +0100

    feat: Implement web security enhancements

---
+++ backend/src/test/java/com/github/moinmarcell/backend/security/AppUserControllerTest.java
@@ -0,0 +1,39 @@
+package com.github.moinmarcell.backend.security;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class AppUserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username = "test")
+    @DisplayName("GET /api/auth/me - Success")
+    void getMe_expectStatus200AndUsername_whenLoggedIn() throws Exception {
+        mockMvc.perform(get("/api/auth/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("test"));
+    }
+
+    @Test
+    @DisplayName("GET /api/auth/me - Unauthorized")
+    void getMe_whenNotLoggedIn_expectStatus401() throws Exception {
+        mockMvc.perform(get("/api/auth/me"))
+                .andExpect(status().isUnauthorized());
+    }
+}
